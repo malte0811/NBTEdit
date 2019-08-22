e0398c3700d660a3422841e13d923c345b55741e
malte0811 <malte0811@web.de>
2019-08-22 08:38:57 +0200

    Build 0.8.1: Support for Forge 28.0.45+

--- src/main/java/malte0811/nbtedit/client/ClientEventHandler.java
+++ src/main/java/malte0811/nbtedit/client/ClientEventHandler.java
@@ -2,13 +2,11 @@ package malte0811.nbtedit.client;
 
 import malte0811.nbtedit.NBTEdit;
 import malte0811.nbtedit.nbt.AutoPullConfig;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.api.distmarker.Dist;
-import malte0811.nbtedit.command.CommandNbtEdit;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 
 import java.util.Iterator;
 
